d572f9c2a72ab7da832bf65f38b21765952215dd
dengguangqing <dengguangqing@cnnic.cn>
2013-12-23 14:15:28 +0800

    IoTNewRuleRecognitionAction.java
    uodate IoTNewRuleRecognitionAction.java

--- source/niot/src/cn/niot/controller/IoTNewRuleRecognitionAction.java
+++ source/niot/src/cn/niot/controller/IoTNewRuleRecognitionAction.java
@@ -81,31 +81,35 @@ public class IoTNewRuleRecognitionAction extends ActionSupport {
 	public String execute() throws Exception
 	{
 		System.out.println(this.len+"!!!!!"+this.valueRange);
-<<<<<<< HEAD
 		
 		String resJasonStr = NewIDstdCollisionDetect.formJsonString(this.len, this.valueRange);
 		HashMap<String, Double> HashMapID2Probability = NewIDstdCollisionDetect.computeCollisionRate(resJasonStr);
+		
+		JSONArray jsonArray = new  JSONArray();
+		double probability = 0;
     	int len = HashMapID2Probability.size();
     	if (RecoUtil.NO_ID_MATCHED == len){
     		this.status = String.valueOf(RecoUtil.NO_ID_MATCHED);
     	} else if (RecoUtil.ONE_ID_MATCHED == len){
     		Iterator iterator = HashMapID2Probability.keySet().iterator();                
             while (iterator.hasNext()) {    
-				Object key = iterator.next();    
-				this.data = String.valueOf(key); 
+				Object key = iterator.next();
 				this.status = String.valueOf(RecoUtil.ONE_ID_MATCHED);
+				JSONObject jsonObject = new  JSONObject();
+				probability = HashMapID2Probability.get(key);
+				jsonObject.put("codeName",String.valueOf(key));
+				jsonObject.put("CollisionRatio",String.valueOf(probability));
+				this.data = jsonObject.toString();
             } 
     	} else {
     		this.status = String.valueOf(len);
-            
-            JSONArray jsonArray = new  JSONArray();
             Iterator iterator2 = HashMapID2Probability.keySet().iterator();                
             while (iterator2.hasNext()) {    
 				Object key = iterator2.next();  				
 				JSONObject jsonObject = new  JSONObject();
-				double probability = HashMapID2Probability.get(key);
+				probability = HashMapID2Probability.get(key);
 				jsonObject.put("codeName",String.valueOf(key));
-				jsonObject.put("probability",String.valueOf(probability));
+				jsonObject.put("CollisionRatio",String.valueOf(probability));
 				if (jsonArray.add(jsonObject)){
 					System.out.println("ERROR! jsonArray.add(jsonObject)");
 				}
@@ -114,43 +118,25 @@ public class IoTNewRuleRecognitionAction extends ActionSupport {
     	}
     	
     	// for test only!
+    	/*
     	this.status = String.valueOf(2);        
-        JSONArray jsonArray = new  JSONArray();
+        JSONArray jsonArray1 = new  JSONArray();
         JSONObject jsonObject = new  JSONObject();             
         jsonObject.put("codeName","b");
-		jsonObject.put("probability",String.valueOf(0.3));
-		jsonArray.add(jsonObject);
+		jsonObject.put("CollisionRatio",String.valueOf(0.3));
+		jsonArray1.add(jsonObject);
 		JSONObject jsonObject2 = new  JSONObject();
 		jsonObject2.put("codeName","a");
-		jsonObject2.put("probability",String.valueOf(0.7));
-=======
-//		
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		
-		//this.status = "7";
-		//this.data = "[{codeName:'cpc',CollisionRatio:0.12},{codeName:'eCode',CollisionRatio:0.88},{codeName:'fCode',CollisionRatio:0.80},{codeName:'mFS',CollisionRatio:0.48},{codeName:'pdAF',CollisionRatio:0.18},{codeName:'Fnme',CollisionRatio:0.88},{codeName:'qqrf',CollisionRatio:0.56}]";
+		jsonObject2.put("CollisionRatio",String.valueOf(0.7));
 		
-		this.status = "1";
-		this.data = "{codeName:'cpc',CollisionRatio:0.12}";
-		
-		//this.status = "error";
-		//this.statement = "服务器响应时间超时";
-		
-		//this.status = "0";
->>>>>>> xiaobaicoding-master
-		
-		jsonArray.add(jsonObject2);
-		this.data = jsonArray.toString();
+		jsonArray1.add(jsonObject2);
+		this.data = jsonArray1.toString();
+		*/
 	
 		//this.status = "7";
 		//this.data = "[{codeName:'cpc',CollisionRatio:0.12},{codeName:'eCode',CollisionRatio:0.88},{codeName:'fCode',CollisionRatio:0.80},{codeName:'mFS',CollisionRatio:0.48},{codeName:'pdAF',CollisionRatio:0.18},{codeName:'Fnme',CollisionRatio:0.88},{codeName:'qqrf',CollisionRatio:0.56}]";
 		
-		System.out.println(this.status+this.data+this.statement);
+		System.out.println(this.status+"\n"+this.data+"\n"+this.statement);
 		return SUCCESS;
 	}
 
