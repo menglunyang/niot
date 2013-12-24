5e6ef054b19c2f2e2b479281bfdb961313d18f70
zt <taozhang828@126.com>
2013-12-24 16:23:20 +0800

    update function 1 for one result; update function 2; change result color

--- source/niot/WebRoot/js/zt_function_1.js
+++ source/niot/WebRoot/js/zt_function_1.js
@@ -1,4 +1,6 @@
 //ztStr = "{status:2,Data:[{codeName:'cpc',probability:12},{codeName:'eCode',probability:88}]}"
+oneresult = Raphael('oneFound_SVG',870, 300);
+
 function sendReqCode()
 {
 	$("#noFound").css("display","none");
@@ -20,6 +22,12 @@ function sendReqCode()
 			data:{code:reqCode},
 			dataType:'json',
 			beforeSend:function(){
+							oneresult.clear();
+							$("#errorFound").css("display","none");
+							$("#noFound").css("display","none");
+							//$("#oneFound").css("display","none");
+							$("#oneFound_SVG").css("display","none");
+							$("#pieChartContainer").css("display","none");
 							$("#loading_1").css("display","block");
 						},
 			success:function(result){
@@ -33,11 +41,23 @@ function sendReqCode()
 								$("#noFound").css("display","block");
 							}
 							else if (result.status == 1){
-								$("#oneFoundName").text(result.data);
-								$("#oneFound").css("display","block");
+								//$("#oneFoundName").text(result.data);
+								//$("#oneFound").css("display","block");
+								
+								$("#oneFound_SVG").css("display","block");
+								
+								oneresult.text(150, 140, "您查询的编码").attr({'font-family':'微软雅黑','font-size':'20px','fill':'#777'});
+								oneresult.circle(380, 140, 140).attr({'fill':'#54A3F0','stroke':''});
+								oneresult.text(380,140,"100%").attr({'font-family':'Lithos Pro','font-size':'130px','fill':'#FFFEFF'});
+								oneresult.text(580,140,"属于"+result.data).attr({'font-family':'微软雅黑','font-size':'20px','fill':'#777'});
 							}
 							else if (result.status >1){
 								var pieData = eval(result.data);
+								var totalNum = pieData.length;
+								for (i=0;i<totalNum;i++)
+									{
+										pieData[i].probability = Number(pieData[i].probability);
+									}
 								$("#pieChartContainer").css("display","block");
 								drawPieChart(pieData);
 							}
@@ -54,6 +74,9 @@ function noFound()
 	$("#noFound").css("display","block");
 }
 
+var myPalette = ['#FC5944', '#F5A349', '#EAE450', '#84E956', '#00EAD3', '#00C1E5','2693E4','6D4EE2','C649E1','FD4497'];
+DevExpress.viz.core.registerPalette('mySuperPalette', myPalette);
+
 function drawPieChart(dataSource)
 {
 	$("#loading").css("display","none");
@@ -61,20 +84,22 @@ function drawPieChart(dataSource)
 	$(function () {
     	$("#pieChartContainer").dxPieChart({
         dataSource: dataSource,
+        palette: 'mySuperPalette',
         size: {
             height: 320,
-            width: 500
+            width: 600
         },
         series: {
-        type:'doughnut',
+       /* type:'doughnut',*/
+        	
             argumentField: 'codeName',
             valueField: 'probability',
             label: {
                 visible: true,
                 format: 'percent',
                 precision: 1,
-                font:{size:6},
-                connector: { visible: true }
+                font:{size:3,weight: 700,color:'#777'},
+                connector: { visible: true },
             }
         },
         title: {text:'Collision Ratio',
@@ -92,7 +117,7 @@ function drawPieChart(dataSource)
        		return argumentText.argument;
    		}
         },
-        animation: {
+      animation: {
             duration: 3000,
             easing: 'linear'
         }
