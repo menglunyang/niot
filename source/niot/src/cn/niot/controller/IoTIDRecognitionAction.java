7450594d1c3adcfe601ed590c89846703a31a967
zt <taozhang828@126.com>
2013-12-26 13:50:51 +0800

    unuseful

--- source/niot/src/cn/niot/controller/IoTIDRecognitionAction.java
+++ source/niot/src/cn/niot/controller/IoTIDRecognitionAction.java
@@ -4,49 +4,29 @@ import cn.unitTest.RuleFuncTest;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-/**
- * 
-* @Title: RespCode.java 
-* @Package cn.niot.zt 
-* @Description:前后台数据传递用例 
-* @author Zhang Tao
-* @date 2013-12-3 上午 
-* @version V1.0
- */
+
 
 public class IoTIDRecognitionAction extends ActionSupport {
 	
-	 /**
-	  * 用户从前台传递来的要查询的编码
-	  */
+
 	private String code;
 	
-	 /**
-	  * 必填。
-	  * 服务器返回给前台的查询状态，
-	  * 允许的取值分别为：“0”，“1”，“大于1的整数”，“error”
-	  */
+
 	private String status;
 	
-	 /**
-	  * 当status取值为“1”或者大于“1”的整数时，必填。
-	  * 服务器返回给前台的编码信息，
-	  * 当status取值为“1”时，data存储查询到的编码名称，例如data="CPC",
-	  * 当status取值为大于1的整数时，data存储编码名称以及定义概率，
-	  * 例如data = "[{codeName:'cpc',probability:0.12},{codeName:'eCode',probability:0.88}]";
-	  */
+
 	private String data;
-	
-	 /**
-	  * 当status=="error"时，必填。
-	  * 服务器返回给前台的错误信息，
-	  * 当status=="error"时，将错误信息赋值给statement，
-	  * 例如statement=="服务器响应超时"，之后传递给前台
-	  */
+	private String extraData;
+
 	private String statement;
 	
 	public String getData() {
 		return data;
+	}
+	
+	
+	public String getExtraData() {
+		return extraData;
 	}
 	
 	public String getStatus() {
@@ -65,28 +45,22 @@ public class IoTIDRecognitionAction extends ActionSupport {
 	public String execute() throws Exception
 	{
 		System.out.println(this.code);
-//		
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		
+	
 		//this.status = "2";
 		//this.data = "[{codeName:'cpc',probability:0.12},{codeName:'eCode',probability:0.88}]";
 		
-		//this.status="5";
-		//this.data = "[{'codeName':'GB/T 19251-2003_EANUCC-8','probability':'0.2'},{'codeName':'GB/T 19251-2003_UCC-12','probability':'0.2'},{'codeName':'Ecode_1','probability':'0.2'},{'codeName':'GB/T 19251-2003_EANUCC-14','probability':'0.2'},{'codeName':'GB/T 19251-2003_EANUCC-13','probability':'0.2'}]";
+		this.status="5";
+		//this.data = "[{'codeName':'GB/T 19251-2003_EANUCC-8asdfsa','probability':'0.2'},{'codeName':'GB/T 19251-2003_UCC-12','probability':'0.2'},{'codeName':'Ecode_1','probability':'0.2'},{'codeName':'GB/T 19251-2003_EANUCC-14','probability':'0.2'},{'codeName':'GB/T 19251-2003_EANUCC-13','probability':'0.2'}]";
 		
-
+		this.data = "[{'codeName':'abc','probability':'0.5399'},{'codeName':'UPC_E','probability':'0.0376'},{'codeName':'Ecode_5','probability':'0.0563'}] ";
+		this.extraData = "{'abc':{'fullName':'abdfdfdfdfdfdfdfdfdfdfdcdefg', 'codeNum': '00132323'},'UPC_E':{'fullName':'ddfffdfd', 'codeNum': 'dfdffdf'},'Ecode_5':{'fullName':'Ecode_5fdfdf', 'codeNum': 'aaaaa'}}";
 		
 		
-		this.status = "1";
-		this.data = "CPC";
+		//this.status = "1";
+		//this.data = "CPCCPCCPCCPCCPCCPCCPCCPCCPCCPCCPC";
 		
 		//this.status = "error";
-		//this.statement = "服务器响应时间超时";
+		//this.statement = "锟斤拷锟斤拷锟斤拷锟斤拷应时锟戒超时";
 		
 		//this.status = "0";
 		
--- source/niot/src/cn/niot/controller/IoTNewRuleRecognitionAction.java
+++ source/niot/src/cn/niot/controller/IoTNewRuleRecognitionAction.java
@@ -2,53 +2,32 @@ package cn.niot.controller;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-/**
- * 
-* @Title: RespRul.java 
-* @Package cn.niot.zt 
-* @Description:前后台数据传递用例 
-* @author Zhang Tao
-* @date 2013-12-16 下午 
-* @version V1.0
- */
+
 
 public class IoTNewRuleRecognitionAction extends ActionSupport {
 	
-	 /**
-	  * 用户从前台传递来的编码长度
-	  */
+
 	private String len;
-	 /**
-	  * 用户从前台传递来的各位取值范围
-	  */
+
 	private String valueRange;
 	
-	 /**
-	  * 必填。
-	  * 服务器返回给前台的查询状态，
-	  * 允许的取值分别为：“0”，“1”，“大于1的整数”，“error”
-	  */
+
 	private String status;
 	
-	 /**
-	  * 当status取值为“1”或者大于“1”的整数时，必填。
-	  * 服务器返回给前台的编码信息，
-	  * 当status取值为“1”时，data存储查询到的编码名称，例如data="CPC",
-	  * 当status取值为大于1的整数时，data存储编码名称以及定义概率，
-	  * 例如data = "[{codeName:'cpc',probability:0.12},{codeName:'eCode',probability:0.88}]";
-	  */
+
 	private String data;
 	
-	 /**
-	  * 当status=="error"时，必填。
-	  * 服务器返回给前台的错误信息，
-	  * 当status=="error"时，将错误信息赋值给statement，
-	  * 例如statement=="服务器响应超时"，之后传递给前台
-	  */
+	private String extraData;
+	
+
 	private String statement;
 	
 	public String getData() {
 		return data;
+	}
+	
+	public String getExtraData() {
+		return extraData;
 	}
 	
 	public String getStatus() {
@@ -72,22 +51,21 @@ public class IoTNewRuleRecognitionAction extends ActionSupport {
 	public String execute() throws Exception
 	{
 		System.out.println(this.len+"!!!!!"+this.valueRange);
-//		
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		逻辑处理。。。。
-//		
+
 		this.status = "7";
-		this.data = "[{codeName:'cpc',CollisionRatio:0.12},{codeName:'eCode',CollisionRatio:0.88},{codeName:'fCode',CollisionRatio:0.80},{codeName:'mFS',CollisionRatio:0.48},{codeName:'pdAF',CollisionRatio:0.18},{codeName:'Fnme',CollisionRatio:0.88},{codeName:'qqrf',CollisionRatio:0.56}]";
+		//this.data = "[{codeName:'cpc',CollisionRatio:0.12},{codeName:'eCode',CollisionRatio:0.88},{codeName:'fCode',CollisionRatio:0.80},{codeName:'mFS',CollisionRatio:0.48},{codeName:'pdAF',CollisionRatio:0.18},{codeName:'Fnme',CollisionRatio:0.88},{codeName:'qqrf',CollisionRatio:0.56}]";
+		
+		this.data = "[{'codeName':'abc','CollisionRatio':'0.5399'},{'codeName':'UPC_E','CollisionRatio':'0.0376'},{'codeName':'Ecode_5','CollisionRatio':'0.0563'}] ";
+		this.extraData = "{'abc':{'fullName':'abdfdfdfdfdfdfdfdfdfdfdcdefg', 'codeNum': '00132323'},'UPC_E':{'fullName':'ddfffdfd', 'codeNum': 'dfdffdf'},'Ecode_5':{'fullName':'Ecode_5fdfdf', 'codeNum': 'aaaaa'}}";
+		//this.data = "[{'codeName':'abcdefghijklmn','CollisionRatio':'0.5399'},{'codeName':'UPC_E','CollisionRatio':'0.0376'}] ";
+		
+		
 		
 		//this.status = "1";
 		//this.data = "{codeName:'cpc',CollisionRatio:0.12}";
 		
 		//this.status = "error";
-		//this.statement = "服务器响应时间超时";
+		//this.statement = "锟斤拷锟斤拷锟斤拷锟斤拷应时锟戒超时";
 		
 		//this.status = "0";
 		
